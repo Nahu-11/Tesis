459e4f71a02aa26cc4da8c42f592350f1a3be157
Nahu-11 <nahuelebautista@hotmail.com>
2019-09-29 18:39:02 -0300

    Update and rename Espectros.py to Old scripts/Espectros.py

--- Old scripts/Espectros.py
+++ Old scripts/Espectros.py
@@ -6,13 +6,15 @@ import os
 from scipy.optimize import curve_fit
 import glob
 
-z=2.85 #Altura de las mediciones
+# Altura de las mediciones
+z=2.85
 
-#Cargo datos
 camino = 'D:\\Mediciones\\Filtrados\\*'
-direccion = glob.glob( camino + '\\ustar.csv') 	#Para calcular los otros espectros cambiar aca
+# Para calcular los otros espectros cambiar aca
+direccion = glob.glob( camino + '\\ustar.csv')
 for j in direccion:
-	guardado = j[0:-9] + 'ustar\\' 				#Para calcular los otros espectros cambiar aca (el -9 tambien)
+	#Para calcular los otros espectros cambiar aca (el -9 tambien)
+	guardado = j[0:-9] + 'ustar\\' 
 	if not os.path.exists(guardado):
 		os.makedirs(guardado)
 	data = pd.read_csv(
@@ -21,8 +23,6 @@ for j in direccion:
 			decimal = '.',
 			engine = 'python'
 			)
-	
-	# Defino algunas variables
 	long = data['Path']
 	max = long.count()
 	fecha = list(range(max))
@@ -34,7 +34,6 @@ for j in direccion:
 	fiteo_b = list(range(max))
 	error_a = list(range(max))
 	error_b = list(range(max))
-	
 	#Abro espectros
 	i = 0
 	for x in long:
@@ -48,21 +47,20 @@ for j in direccion:
 				index_col = False,
 				engine = 'python'
 				)
-		
-		#Calculo los numeros pi
+		# Calculo los numeros pi
 		obukhov[i] = data['1/L'].iloc[i]
 		u_medio[i] = data['u[m/s]         '].iloc[i]
 		u_star[i] = data['ustar[m/s]     '].iloc[i]
 		frec = espectro[' f']
 		intensidad = espectro['Su']
-													#Para calcular los otros espectros cambiar aca (el fi_e)
+		# Para calcular los otros espectros cambiar aca (el fi_e)
 		if obukhov[i] < 0:
 			fi_e= 1 + 0.5 * abs(z*obukhov[i]) **(2/3)
 		else:
 			fi_e = (1 + 5 * abs(z*obukhov[i])) **(2/3)
 		espectro['pi_1'] = frec * z / u_medio[i]
-		espectro['pi_u'] = frec * intensidad / (u_star[i]**2 * fi_e) #fi_e ya está elevado a la dos tercios, por eso no lo hago
-		
+		# fi_e ya está elevado a la dos tercios, por eso no lo hago
+		espectro['pi_u'] = frec * intensidad / (u_star[i]**2 * fi_e)
 		#Ajuste de los datos
 		def potencial(a,b,c):
 			return b*a**c
@@ -79,7 +77,6 @@ for j in direccion:
 		fiteo_b[i] = fiteo[1]
 		error_a[i] = error[0]
 		error_b[i] = error[1]
-		
 		#Hago los graficos
 		plt.figure(figsize=(18,9))
 		plt.plot(espectro['pi_1'], espectro['pi_u'],
@@ -87,18 +84,23 @@ for j in direccion:
 				ms = 5, marker = '.')
 		plt.plot(x_fit,	ajuste,
 				'r-', lw = 3,
-				label = ' a = '+ str('{:.2e}'.format(fiteo[0])) +
-						' std_a = ' + str('{:.2e}'.format(error[0])) + '\n' + 
-						' b = ' + str('{:.2e}'.format(fiteo[1])) +
-						' std_b = ' + str('{:.2e}'.format(error[1])) + '\n' + 
-						' R^2 = ' + str(round(r_cuadrado[i],2))
+				label = (' a = '+ str('{:.2e}'.format(fiteo[0]))
+					+ ' std_a = ' + str('{:.2e}'.format(error[0])) + '\n'
+					+ ' b = ' + str('{:.2e}'.format(fiteo[1]))
+					+ ' std_b = ' + str('{:.2e}'.format(error[1])) + '\n'
+					+ ' R^2 = ' + str(round(r_cuadrado[i],2))
+					)
 				)
 		plt.yscale('log')
 		plt.xscale('log')
 		plt.axis([0.001, 10, 0.000001, 10])
-		plt.title(nombre + " 1/L = " + str('{:.2e}'.format(obukhov[i])) + " Uo = " + str('{:.2e}'.format(u_star[i])))
+		plt.title(nombre + " 1/L = "
+			  + str('{:.2e}'.format(obukhov[i]))
+			  + " Uo = " + str('{:.2e}'.format(u_star[i]))
+			 )
 		plt.xlabel("f*z/U")
-		plt.ylabel("f*Su/(Uo^2*fi-e)")				#Para calcular los otros espectros cambiar fi-e
+		# Para calcular los otros espectros cambiar fi-e
+		plt.ylabel("f*Su/(Uo^2*fi-e)")				
 		plt.legend()
 		plt.savefig(guardado + nombre + '.png')
 		plt.close()
@@ -108,7 +110,8 @@ for j in direccion:
 		'Fecha' : fecha,
 		'1/L' : obukhov,
 		'U' : u_medio,
-		'Uo' : u_star,								#Para calcular los otros espectros tambien cambiar aca
+		# Para calcular los otros espectros tambien cambiar aca
+		'Uo' : u_star,								
 		'a' : fiteo_a,
 		'std_a' : error_a,
 		'b' : fiteo_b,
@@ -116,8 +119,6 @@ for j in direccion:
 		'R^2' : r_cuadrado
 		}
 	tabla_final = pd.DataFrame(data = tabla,)
-	#Guardo la tabla
 	tabla_final.to_csv(path_or_buf = guardado + 'Estadisticos.csv', index = False)
-
 print('Todo salio bien')
 input()

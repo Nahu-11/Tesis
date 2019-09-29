703ddfca7b31b081f9d1359db23428ae1d99e37d
Nahu-11 <nahuelebautista@hotmail.com>
2019-09-29 18:41:55 -0300

    Update and rename Grafico_y_estadisticos.py to Old scripts/Grafico_y_estadisticos.py

--- Old scripts/Grafico_y_estadisticos.py
+++ Old scripts/Grafico_y_estadisticos.py
@@ -1,10 +1,8 @@
-#Importo librerias
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
 
-#Variables que voy a usar
 columnas = ["Momento","TSS1","TSS2","TS1","TS2","TS3","TS4","TS_QG","U","DIR",
 "TA1","HR1","TA2","HR2","TA3","HR3","TA4","HR4","QG1","QG2","WET","RG","PAR",
 "TSH1","HS1","GH1","Real(KH)1","Imag(KH)1","Real_T(KH)1","Imag_T(KH)1","TSH2",
@@ -14,7 +12,6 @@ columnas = ["Momento","TSS1","TSS2","TS1","TS2","TS3","TS4","TS_QG","U","DIR",
 "Imag(KH)5","Real_T(KH)5","Imag_T(KH)5"]
 cant_col = len(columnas)
 
-#Cargo los datos
 data = pd.read_csv(
     'C:\\Users\\nahue\\Desktop\\Tesis\\analisis.csv',
     delimiter = ';',
@@ -27,7 +24,9 @@ data = pd.read_csv(
     )
 
 #Defino variables que voy a usar
-dias = [32, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
+dias = [32, 60, 91, 121, 152, 182, 213,
+	244, 274, 305, 335, 366
+       ]
 cant_dias = len(dias)
 mes = list(range(cant_dias - 1))
 total = list(range(cant_dias - 1))
@@ -41,12 +40,9 @@ curtosis = list(range(cant_dias - 1))
 j=1
 for y in columnas:
 	if j < cant_col:
-		
-		#Creo directorios para guardar los gráficos y tablas según variable
 		direccion = 'C:\\Users\\nahue\\Desktop\\Tesis\\Resultados\\' + columnas[j]
 		if not os.path.exists(direccion):
 			os.makedirs(direccion)
-		
 		maximo = data[columnas[j]].max()
 		minimo = data[columnas[j]].min()
 		#Termino de separar los datos por mes
@@ -56,7 +52,6 @@ for y in columnas:
 				inicial = dias[i]
 				final = dias[i+1] - 1
 				variable = data[columnas[j]].loc[inicial:final]
-				
 				#Calculo estadisticos
 				mes[i]= str(inicial) + '-' + str(final)
 				total[i] = variable.count()
@@ -65,7 +60,6 @@ for y in columnas:
 				varianza[i] = variable.var()
 				asimetria[i] = variable.skew()
 				curtosis[i] = variable.kurtosis()
-				
 				#Grafico para cada variable según el mes
 				variable.plot(
 					x ='Día',
@@ -74,18 +68,15 @@ for y in columnas:
 					title = 'Días ' + mes[i],
 					xlim = (inicial, final),
 					ylim = (minimo, maximo),
-					style = '.').set(
-						xlabel = "Momento Juliano",
-						ylabel = columnas[j]) 
-				
+					style = '.'
+				).set(xlabel = "Momento Juliano",
+				      ylabel = columnas[j]
+				      ) 
 				del variable
-				
-				#Guardo los gráficos
 				k = str(i + 2)
 				plt.savefig(direccion + '\\' + k + ' Dias ' + mes[i] + '.png')
 				plt.close()
 			i = i + 1
-		
 		#Creo una tabla de estadisticos para cada mes
 		tabla = {
 			'Dias': mes,
@@ -96,17 +87,10 @@ for y in columnas:
 			'Asimetría':asimetria,
 			'Curtosis': curtosis}
 		tabla_final = pd.DataFrame(data = tabla,)
-		
-		#Guardo la tabla
 		tabla_final.to_csv(path_or_buf = direccion + '\\Estadisticos.csv', index = False)
 	j = j + 1
-
 del data
 del tabla_final
-
-#Este es un comentario inútil
-# print('El script se ha ejecutado con éxito, presione una tecla para salir')
-# input()
 
 #Ejecuta el script para la Precipitacion
 exec(open('C:\\Users\\nahue\\Desktop\\Tesis\\Precipitacion.py').read())
